75d03602cb67c02f8ae66d2baf477e85c266a7ec
Gilbert <gilbertcurbelo3@aol.com>
2019-02-04 12:41:12 -0800

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Gibby on 2/4/19.
+//  Copyright © 2019 gcapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
