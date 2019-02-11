c10793bfbcfa142ed52f8882db9372e22122e58e
Gilbert <gilbertcurbelo3@aol.com>
2019-02-11 12:35:07 -0800

    Flixter 2

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Gibby on 2/11/19.
+//  Copyright © 2019 gcapp. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
